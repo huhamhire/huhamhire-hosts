2a4031c89500057facee8028f075a0ad6f1fab7c
Weihong.Guan <weihong.Guan@gmail.com>
2013-02-04 18:18:12 +0800

    convert decode of hostsx and adblok to utf-8

--- modules/build.sh
+++ modules/build.sh
@@ -8,7 +8,6 @@ cat info.hosts timestamp.hosts localhost.hosts > tmp/header.hosts.tmp
 for folder in $(ls -d *_mods)
 do
 #	echo $folder
-
 	>tmp/$folder.hosts.tmp
 	for file in $(ls $folder/)
 	do
@@ -18,5 +17,11 @@ done
 
 cat tmp/header.hosts.tmp tmp/share_mods.hosts.tmp tmp/ipv4_mods.hosts.tmp > tmp/main_ipv4.hosts
 cat tmp/header.hosts.tmp tmp/share_mods.hosts.tmp tmp/ipv6_mods.hosts.tmp > tmp/main_ipv6.hosts
+cat tmp/adblock_mods.hosts.tmp > tmp/adblock.hosts
 
 rm tmp/*.tmp
+
+cat tmp/main_ipv4.hosts > ../downloads/raw/ipv4_mobile_utf8/hosts
+cat tmp/main_ipv4.hosts > ../downloads/raw/ipv4_unix_utf8/hosts
+cat tmp/main_ipv4.hosts tmp/adblock.hosts | iconv -f utf-8 -t GBK > ../downloads/raw/ipv4_win_ansi/hosts
+
