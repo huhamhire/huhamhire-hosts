cfe43ad83466c79a83b22c94ffd34f0d8c52f2f7
huhamhire <me@huhamhire.com>
2013-11-23 11:39:50 +0800

    update module to 1.0.1: 1.fix bug while creating hosts id; 2.update datafile to 1.3.9.
    Signed-off-by: huhamhire <me@huhamhire.com>

--- makedata.py
+++ makedata.py
@@ -14,7 +14,7 @@
 # PURPOSE.
 # =====================================================================
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __revision__ = "$Id$"
 __author__ = "huhamhire <me@huhamhire.com>"
 
@@ -127,7 +127,7 @@ class MakeData(object):
             items = line.split()
             if len(items) < 2:
                 continue
-            index = hashlib.md5(items[1]).hexdigest()[:8]
+            index = hashlib.md5(items[1] + str(category)).hexdigest()[:8]
             index = int(index, 16)
             host = (index, category, items[0], items[1])
             try:
