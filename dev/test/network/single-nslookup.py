8d33e56a06aa3e557c45ae2018fd24ed3ae465c3
huhamhire <me@huhamhire.com>
2014-07-27 10:20:36 +0800

    update modules
    1. add OneDrive support
    2. update twitter list
    3. add iCloud support
    4. add more dropbox servers

--- dev/test/network/single-nslookup.py
+++ dev/test/network/single-nslookup.py
@@ -50,10 +50,11 @@ class TestNSLookup(QThread):
             "sa": "194.183.88.40",
 
             "test": "208.67.222.222",
+            # "open": "42.120.21.30"
         }
 
-        filters = {"share-apple": ["sa"]}
-        domains = {"share-apple": ["www.google.com"]}
+        filters = {"share-apple": ["us"]}
+        domains = {"share-apple": ["www.google.com.hk"]}
         v6 = False
 
         lookups = MultiNSLookup(ns_servers, filters, domains, self.logger, v6)
